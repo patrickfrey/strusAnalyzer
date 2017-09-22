ec25fb5dc5bf18cd7ac5c85031bce8d23a23f2c9
patrickfrey <patrickpfrey@yahoo.com>
2017-09-22 12:41:27 +0200

    take .travis.yml out of gitignore

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -9,8 +9,7 @@ case $OS in
 		sudo apt-get update -qq
 		sudo apt-get install -y \
 			cmake \
-			libboost-all-dev \
-			libleveldb-dev
+			libboost-all-dev
 		;;
 		
 	Darwin)
@@ -28,15 +27,6 @@ case $OS in
 			|| true
 		# make sure cmake finds the brew version of gettext
 		brew link --force gettext || true
-		brew link leveldb || true
-		brew link snappy || true
-		# rebuild leveldb to use gcc-4.8 ABI on OSX (libstc++ differs
-		# from stdc++, leveldb uses std::string in API functions, C
-		# libraries like gettext and snappy and even boost do not 
-		# have this problem)
-		if test "X$CC" = "Xgcc"; then
-			brew reinstall leveldb --cc=gcc-4.8
-		fi
 		;;
 	
 	*)
