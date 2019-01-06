49b3aeeef0e50e7f85484e41ffcf7900705b538c
PatrickFrey <patrickpfrey@yahoo.com>
2019-01-06 17:10:37 +0100

    changed some comments and error messages

--- include/strus/lib/aggregator_vsm.hpp
+++ include/strus/lib/aggregator_vsm.hpp
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-/// \brief Exported functions of the strus analyzer vector space model aggregator functions library
+/// \brief Exported functions of the strus analyzer vector space aggregator functions library
 /// \file aggregator_vsm.hpp
 #ifndef _STRUS_ANALYZER_AGGREGATOR_VSM_LIB_HPP_INCLUDED
 #define _STRUS_ANALYZER_AGGREGATOR_VSM_LIB_HPP_INCLUDED
