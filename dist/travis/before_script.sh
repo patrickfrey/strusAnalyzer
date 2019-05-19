e4dd88f75a96e0f80a08cc3a4b67e35e01eae330
PatrickFrey <patrickpfrey@yahoo.com>
2019-05-19 18:13:18 +0200

    updated travis build scripts, switch from trusty to xenial

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -7,12 +7,9 @@ OS=$(uname -s)
 case $OS in
 	Linux)
 		sudo apt-get update -qq
-		sudo apt-get install -y cmake
-		sudo add-apt-repository -y ppa:kojoley/boost
-		sudo apt-get -q update
-		sudo apt-get install libboost-atomic1.58-dev libboost-thread1.58-dev libboost-system1.58-dev libboost-filesystem1.58-dev libboost-regex1.58-dev
+		sudo apt-get install -y cmake libboost-all-dev
 		;;
-		
+
 	Darwin)
 		brew update
 		if test "X$CC" = "Xgcc"; then
