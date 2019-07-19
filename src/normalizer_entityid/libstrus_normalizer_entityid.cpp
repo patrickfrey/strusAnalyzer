008c1499eb6807cf942b7f789f123f5269b45387
PatrickFrey <patrickpfrey@yahoo.com>
2019-07-19 07:38:21 +0200

    fixed bad include

--- src/normalizer_entityid/libstrus_normalizer_entityid.cpp
+++ src/normalizer_entityid/libstrus_normalizer_entityid.cpp
@@ -6,7 +6,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 #include "strus/lib/normalizer_entityid.hpp"
-#include "strus/analyzerModule.hpp"
 #include "strus/base/dll_tags.hpp"
 #include "strus/normalizerFunctionInterface.hpp"
 #include "strus/normalizerFunctionInstanceInterface.hpp"
